9c3af0e53f365866e73ac8e4e7c5719531e92430
aronfan <aronfan@vip.qq.com>
2021-01-23 13:19:20 +0800

    add Fonts for wow taiwan

---
+++ Fonts/font.bat
@@ -0,0 +1,20 @@
+@echo off
+
+echo begin...
+# gold/key
+copy 1.ttf FRIZQT__.TTF
+# hp/exp
+copy 1.ttf ARIALN.TTF
+# chat
+copy 1.ttf ARHei.TTF
+# combat dmg
+copy 1.ttf ARKai_C.TTF
+# ui
+copy 1.ttf ARKai_T.TTF
+# quest
+copy 1.ttf FZBWJW.TTF
+# item/spell
+copy 1.ttf FZXHJW.TTF
+# custom
+copy 1.ttf YourFontName.TTF
+echo finished!
---
+++ Fonts/font.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo begin...
+# gold/key
+cp 1.ttf FRIZQT__.TTF
+# hp/exp
+cp 1.ttf ARIALN.TTF
+# chat
+cp 1.ttf ARHei.TTF
+# combat dmg
+cp 1.ttf ARKai_C.TTF
+# ui
+cp 1.ttf ARKai_T.TTF
+# quest
+cp 1.ttf FZBWJW.TTF
+# item/spell
+cp 1.ttf FZXHJW.TTF
+# custom
+cp 1.ttf YourFontName.TTF
+echo finished!
